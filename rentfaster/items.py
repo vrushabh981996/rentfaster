f8a24565a37dd66223bb6f8a3322c18bbe9a77fc
vrushabh981996 <103637956+vrushabh981996@users.noreply.github.com>
2024-03-12 18:18:55 +0530

    Add files via upload

--- rentfaster/items.py
+++ rentfaster/items.py
@@ -9,4 +9,10 @@ import scrapy
 class RentfasterItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    WebLink = scrapy.Field()
+    ListingID = scrapy.Field()
+    Type = scrapy.Field()
+    Price = scrapy.Field()
+    Address = scrapy.Field()
+    Number = scrapy.Field()
     pass
